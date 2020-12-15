2691054652e6e6c43d3a19ab15e3abbb715579e6
nbutluo <nbutluo@163.com>
2020-12-15 09:38:23 +0800

    修复后台底部footer 跳转链接

--- resources/views/admin/layouts/_footer.blade.php
+++ resources/views/admin/layouts/_footer.blade.php
@@ -1,4 +1,4 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-      <strong>Copyright &copy; 2020-<a href="https://www.loctekmotion.com/" target="_blank">LoctekMotion</a></strong>
+    <strong>Copyright &copy; 2020-<a href="{{ route('root') }}" target="_blank">LoctekMotion</a></strong>
   </footer>
