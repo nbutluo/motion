6f3b6fc71bdbc7b8ccfeddd3b829c8411fae38cb
nbutluo <nbutluo@163.com>
2020-12-21 10:36:21 +0800

    Revert "后台首页跳转到用户列表页"
    This reverts commit 73247ce23b37da8abe1a64c65c149206b4da755e.

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -15,6 +15,46 @@ class IndexController extends Controller
 
     public function index()
     {
-        return view('admin.users.index');
+        return view('admin.indexs.index');
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Admin $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Admin $admin)
+    {
+        //
+    }
+
+    public function destroy(Admin $admin)
+    {
+        //
     }
 }
