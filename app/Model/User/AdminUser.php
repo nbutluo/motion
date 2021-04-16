519133ce4b2d01f999f6f122e2a1ce5cb73ab993
cq01 <872029501@qq.com>
2021-04-16 13:50:33 +0800

    添加lavarel-ide-helper

--- app/Model/User/AdminUser.php
+++ app/Model/User/AdminUser.php
@@ -4,6 +4,30 @@ namespace App\Model\User;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\User\AdminUser
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string|null $api_token
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AdminUser whereUsername($value)
+ * @mixin \Eloquent
+ */
 class AdminUser extends Model
 {
     protected $table = 'admin_users';
--- app/User.php
+++ app/User.php
@@ -6,6 +6,54 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $username 用户名
+ * @property string $password 密码
+ * @property string $nickname
+ * @property int $user_leverl 用户等级：0普通用户，1一级用户，2二级用户
+ * @property int $sex 性别：0女，1男
+ * @property string $birth 生日
+ * @property string $email email
+ * @property string $phone
+ * @property string $province
+ * @property string $city
+ * @property string $area
+ * @property string $company_url
+ * @property string|null $remember_token
+ * @property string $api_token
+ * @property string|null $deleted_at
+ * @property string $last_login 上次登录时间
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereArea($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBirth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCompanyUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLogin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereNickname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProvince($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSex($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUserLeverl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUsername($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
