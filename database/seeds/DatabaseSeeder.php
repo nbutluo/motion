36e93e95e5e3c8129e2b1a47994e55b1843b9879
cq01 <872029501@qq.com>
2021-04-23 17:05:27 +0800

    删除website_seo表数据填充

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
             ConfigurationTableSeeder::class,
             CategoriesTableSeeder::class,
             FooterLinksTableSeeder::class,
-            WebSiteSeeder::class,
         ]);
     }
 }
