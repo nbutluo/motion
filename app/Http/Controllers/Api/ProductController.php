b6f94ce749ac46385348e8d9d321efd20d3860f3
cq01 <872029501@qq.com>
2021-05-17 17:49:44 +0800

    产品接口图片路径加上域名

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -43,6 +43,15 @@ class ProductController extends ApiController
 
             $data = $this->productModel->getPageList($page, $pageSize, $where, $select);
 
+            foreach ($data['list'] as $list) {
+                if ($list->image != '') {
+                    $list->image = $_SERVER["HTTP_HOST"].$list->image;
+                }
+                if ($list->small_image != '') {
+                    $list->small_image = $_SERVER["HTTP_HOST"].$list->small_image;
+                }
+            }
+
             return $this->success('success', $data);
         } catch (Exception $e) {
             return $this->fail('error, failure.', 500);
@@ -56,12 +65,32 @@ class ProductController extends ApiController
         ];
 
         $data = $this->productModel->getDetailForApi($id, $select);
-
+        if ($data['image'] !='') {
+            $data['image'] = $_SERVER["HTTP_HOST"].$data['image'];
+        }
         if (!empty($data)) {
             // option 选项配置
-            $data['option_size'] = $this->optionModel->where('product_id', $data['id'])->where('type', 1)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
-            $data['option_color'] = $this->optionModel->where('product_id', $data['id'])->where('type', 2)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
-            $data['desk_img'] = $this->optionModel->where('product_id', $data['id'])->where('type', 3)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
+            $option_size = $this->optionModel->where('product_id', $data['id'])->where('type', 1)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
+            foreach ($option_size as $o_s) {
+                if ($o_s['image'] != '') {
+                    $o_s['image'] = $_SERVER["HTTP_HOST"].$o_s['image'];
+                }
+            }
+            $data['option_size'] = $option_size;
+            $option_color = $this->optionModel->where('product_id', $data['id'])->where('type', 2)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
+            foreach ($option_color as $o_c) {
+                if ($o_c['image'] !== '') {
+                    $o_c['image'] = $_SERVER["HTTP_HOST"].$o_c['image'];
+                }
+            }
+            $data['option_color'] = $option_color;
+            $desk_img = $this->optionModel->where('product_id', $data['id'])->where('type', 3)->where('is_active', 1)->orderBy('sort_order', 'desc')->get();
+            foreach ($desk_img as $d_i) {
+                if ($d_i['image'] != '') {
+                    $d_i['image'] = $_SERVER["HTTP_HOST"].$d_i['image'];
+                }
+            }
+            $data['desk_img'] = $desk_img;
         }
 
         return $this->success('success', $data);
