ac320d224c782ff70a7a9c6265df3932d0b84bef
cq01 <872029501@qq.com>
2021-06-04 13:40:40 +0800

    修改banner接口，整合pc端与手机端，banner变为多图模式

--- app/Http/Controllers/Api/BannerController.php
+++ app/Http/Controllers/Api/BannerController.php
@@ -11,11 +11,26 @@ class BannerController extends ApiController
     public function getBanner($pageName)
     {
         try {
-            $banner = MediumBanner::where('is_active',1)->where('page_name','like',$pageName.'%')->get();
+            $data = [
+                'media_img' => [],
+                'mob_img' => []
+            ];
+            $banner = MediumBanner::select(['page_name','media_url'])->where('is_active',1)->where('page_name','like',$pageName.'%')->get();
             foreach ($banner as $ban) {
-                $ban->media_url = HTTP_TEXT.$_SERVER["HTTP_HOST"].$ban->media_url;
+                if (isset($ban->media_url) && $ban->media_url != '') {
+                    $medias = explode(';',$ban->media_url);
+                    $mediaData = [];
+                    foreach ($medias as $media) {
+                        $mediaData[] = HTTP_TEXT.$_SERVER["HTTP_HOST"].$media;
+                    }
+                    if (strpos($ban->page_name,'mobile') !== false) {
+                        $data['mob_img'] = $mediaData;
+                    } else {
+                        $data['media_img'] = $mediaData;
+                    }
+                }
             }
-            return $this->success('success', $banner);
+            return $this->success('success', $data);
         } catch (\Exception $exception) {
             return $this->fail($exception->getMessage(), 404, []);
         }
