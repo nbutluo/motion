471a137a9996baacb3e14feae1b47d67209b348f
nbutluo <nbutluo@163.com>
2021-11-15 18:27:03 +0800

    后台配件库按照更新时间倒序

--- app/Model/Product/Option.php
+++ app/Model/Product/Option.php
@@ -9,7 +9,7 @@ class Option extends Model
 {
     protected $table = 'catalog_product_option';
     protected $primaryKey = 'id';
-    protected $fillable = ['sku','product_id','title','type','description','is_active','sort_order','image','image_alt','image_alt','option_color','option_size'];
+    protected $fillable = ['sku', 'product_id', 'title', 'type', 'description', 'is_active', 'sort_order', 'image', 'image_alt', 'image_alt', 'option_color', 'option_size'];
 
     //考虑到性能问题，通常$columns我们不以*号为值。可传入需要查询的字段替代。这里只做演示。无此要求
     public function paginate($perPage = null, $columns = ['*'], $page = null, $pageName = 'page', $where = [])
@@ -34,7 +34,7 @@ class Option extends Model
             }
         } else {
             if ($total = $this->toBase()->getCountForPagination()) {
-                $results = $this->forPage($page, $perPage)->get($columns);
+                $results = $this->orderBy('updated_at', 'desc')->forPage($page, $perPage)->get($columns);
             } else {
                 $results = [];
             }
