f9547a94ea25d5e629747577504398e781d1db4c
nbutluo <nbutluo@163.com>
2022-01-28 10:15:10 +0800

    修复提示token错误问题

--- app/Http/Controllers/Api/AddToCartController.php
+++ app/Http/Controllers/Api/AddToCartController.php
@@ -17,15 +17,15 @@ class AddToCartController extends ApiController
     {
         try {
             $token = $request->header('Authorization');
-            $user = Users::where('api_token',$token)->first();
-            $order = json_decode($request->item,true);
+            $user = Users::where('api_token', $token)->first();
+            $order = json_decode($request->item, true);
             $data = [];
             $data['product_id'] = $order['product_id'];
             $data['user_id'] = (isset($user) && !empty($user)) ? $user->id : 0;
             $data['qty'] = $order['qty'];
             $option_text = '';
             foreach ($order['options'] as $option) {
-                $option_text = ($option_text == '') ? $option['option_id'] : $option_text.','.$option['option_id'];
+                $option_text = ($option_text == '') ? $option['option_id'] : $option_text . ',' . $option['option_id'];
             }
             $data['options'] = $option_text;
             $data['token'] = $token;
@@ -41,24 +41,24 @@ class AddToCartController extends ApiController
         try {
             $data = [];
             $token = $request->header('Authorization');
-            $user = Users::where('api_token',$token)->first();
+            $user = Users::where('api_token', $token)->first();
             if (isset($user) && !empty($user)) {
-                $carts = AddToCart::where('user_id',$user->id)->get();
+                $carts = AddToCart::where('user_id', $user->id)->get();
             } else {
-                $carts = AddToCart::where('token',$token)->get();
+                $carts = AddToCart::where('token', $token)->get();
             }
             foreach ($carts as $cart) {
-                $product = Product::select(['id','name','sku','image'])->findOrFail($cart->product_id);
+                $product = Product::select(['id', 'name', 'sku', 'image'])->findOrFail($cart->product_id);
                 $product->cart_id = $cart->id;
                 $product->qty = $cart->qty;
-                $productImages = explode(';',$product->image);
-                $product->image = HTTP_TEXT.$_SERVER["HTTP_HOST"].$productImages[0];
-                $optionIds = explode(',',$cart->options);
+                $productImages = explode(';', $product->image);
+                $product->image = HTTP_TEXT . $_SERVER["HTTP_HOST"] . $productImages[0];
+                $optionIds = explode(',', $cart->options);
                 $optionData = [];
                 foreach ($optionIds as $optionId) {
-                    $option = Option::select(['id','sku','title','type','image','option_color','option_size'])->findOrFail($optionId);
+                    $option = Option::select(['id', 'sku', 'title', 'type', 'image', 'option_color', 'option_size'])->findOrFail($optionId);
                     if (isset($option->image) && !empty($option->image)) {
-                        $option->image = HTTP_TEXT.$_SERVER["HTTP_HOST"].$option->image;
+                        $option->image = HTTP_TEXT . $_SERVER["HTTP_HOST"] . $option->image;
                     }
                     $product->cart_option_color = '';
                     $product->cart_option_size = '';
@@ -75,27 +75,27 @@ class AddToCartController extends ApiController
                 }
 
                 //获取所有配件信息
-//                $alloptions = Option::select(['id','type','sku','title','image','option_color','option_size'])
-//                    ->where('product_id',$cart->product_id)
-//                    ->where('is_active',1)
-//                    ->get();
-//                $typeData1 = [];  $typeData3 = [];
-//                foreach ($alloptions as $alloption) {
-//                    if ($alloption->type == 1) {
-//                        $typeData1[] = $alloption;
-//                        $typeData2 = [];
-//                        foreach ($alloptions as $alloptionSize) {
-//                            if ($alloptionSize->type == 2 && $alloptionSize->option_color == $alloption->option_color) {
-//                                $typeData2[] = $alloptionSize;
-//                            }
-//                        }
-//                        $alloption->option_size = $typeData2;
-//                    } elseif ($alloption->type == 3) {
-//                        $typeData3[] = $alloption;
-//                    }
-//                }
-//                $product->option_color = $typeData1;
-//                $product->desk_img = $typeData3;
+                //                $alloptions = Option::select(['id','type','sku','title','image','option_color','option_size'])
+                //                    ->where('product_id',$cart->product_id)
+                //                    ->where('is_active',1)
+                //                    ->get();
+                //                $typeData1 = [];  $typeData3 = [];
+                //                foreach ($alloptions as $alloption) {
+                //                    if ($alloption->type == 1) {
+                //                        $typeData1[] = $alloption;
+                //                        $typeData2 = [];
+                //                        foreach ($alloptions as $alloptionSize) {
+                //                            if ($alloptionSize->type == 2 && $alloptionSize->option_color == $alloption->option_color) {
+                //                                $typeData2[] = $alloptionSize;
+                //                            }
+                //                        }
+                //                        $alloption->option_size = $typeData2;
+                //                    } elseif ($alloption->type == 3) {
+                //                        $typeData3[] = $alloption;
+                //                    }
+                //                }
+                //                $product->option_color = $typeData1;
+                //                $product->desk_img = $typeData3;
                 $data[] = $product;
             }
             return $this->success('success', $data);
@@ -112,12 +112,12 @@ class AddToCartController extends ApiController
             }
             $data = [];
             $token = $request->header('Authorization');
-            $user = Users::where('api_token',$token)->first();
+            $user = Users::where('api_token', $token)->first();
             if (!isset($user) || empty($user)) {
                 throw new \Exception('please login!');
             }
 
-            $items = json_decode($request->items,true);
+            $items = json_decode($request->items, true);
             foreach ($items as $item) {
                 $catMsg = AddToCart::findOrFail($item['cart_id']);
                 if (!isset($catMsg) || empty($catMsg)) {
@@ -130,11 +130,11 @@ class AddToCartController extends ApiController
                     throw new \Exception('cart message is wrong!');
                 }
                 $catMsg->qty = $item['qty'];
-//                $options = '';
-//                foreach ($item['options'] as $option) {
-//                    $options = ($options == '') ? $option['option_id'] : $options.','.$option['option_id'];
-//                }
-//                $catMsg->options = $options;
+                //                $options = '';
+                //                foreach ($item['options'] as $option) {
+                //                    $options = ($options == '') ? $option['option_id'] : $options.','.$option['option_id'];
+                //                }
+                //                $catMsg->options = $options;
                 $catMsg->save();
             }
             return $this->success('success', $data);
@@ -148,12 +148,12 @@ class AddToCartController extends ApiController
         try {
             $data = [];
             $token = $request->header('Authorization');
-            $user = Users::where('api_token',$token)->first();
+            $user = Users::where('api_token', $token)->first();
             $carts = AddToCart::findOrFail($request->cart_id);
             if ((isset($user) && $user->id == $carts->user_id) || $token == $carts->token) {
                 $destoryCart = AddToCart::findOrFail($request->cart_id);
                 $destoryCart->delete();
-                if ($destoryCart->trashed()){
+                if ($destoryCart->trashed()) {
                     $data['cart_id'] = $request->cart_id;
                     return $this->success('success', $data);
                 } else {
@@ -173,7 +173,7 @@ class AddToCartController extends ApiController
         try {
             $categories = Category::all();
             $allCateData = [];
-//            $categories = Category::where('parent_id',1)->where('is_active',1)->get();
+            //            $categories = Category::where('parent_id',1)->where('is_active',1)->get();
             $categoryData = [];
             foreach ($categories as $category) {
                 if ($category->parent_id == 1 && $category->is_active == 1) {
@@ -181,7 +181,12 @@ class AddToCartController extends ApiController
                 }
                 $allCateData[$category->id] = $category->toArray();
             }
-            $relates = Product::select(['id','name','sku','category_id','short_description','image'])->whereIn('category_id',$categoryData)->orderBy('position','DESC')->limit(6)->get();
+            $relates = Product::select(['id', 'name', 'sku', 'category_id', 'short_description', 'image'])->whereIn('category_id', $categoryData)->orderBy('position', 'DESC')->limit(6)->get();
+
+            $relates = $relates->each(function ($product) {
+                return $product->category_id = $product->category->id;
+            });
+
             foreach ($relates as $relate) {
                 //添加分类信息
                 if ($relate->category_id != 0) {
@@ -200,15 +205,15 @@ class AddToCartController extends ApiController
                 }
                 if (isset($relate->image) && $relate->image != '') {
                     $imageData = [];
-                    $images = explode(';',$relate->image);
+                    $images = explode(';', $relate->image);
                     foreach ($images as $image) {
-                        $imageData[] = HTTP_TEXT.$_SERVER["HTTP_HOST"].$image;
+                        $imageData[] = HTTP_TEXT . $_SERVER["HTTP_HOST"] . $image;
                     }
                     $relate->image = $imageData;
                 }
                 $relate->url_key = '';
-                if (isset($relate->id) && isset($productSite['/loctek/product/info/'.$relate->id])) {
-                    $relate->url_key = $productSite['/loctek/product/info/'.$relate->id];
+                if (isset($relate->id) && isset($productSite['/loctek/product/info/' . $relate->id])) {
+                    $relate->url_key = $productSite['/loctek/product/info/' . $relate->id];
                 }
             }
             return $this->success('success', $relates);
@@ -219,7 +224,7 @@ class AddToCartController extends ApiController
     public function getSiteMapForProduct()
     {
         $productSiteMap = [];
-        $siteMaps = Sitemap::select(['url','origin'])->where('type',8)->get();
+        $siteMaps = Sitemap::select(['url', 'origin'])->where('type', 8)->get();
         if ($siteMaps) {
             foreach ($siteMaps as $siteMap) {
                 $productSiteMap[$siteMap->origin] = $siteMap->url;
